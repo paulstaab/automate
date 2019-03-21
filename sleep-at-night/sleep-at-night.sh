0eda4e2db74509c1a20a980f9c663882591cc841
Paul Staab <paul.staab@huawei.com>
2019-03-21 16:54:39 +0100

    Add protection for sending the system to sleep to early

--- sleep-at-night/sleep-at-night.sh
+++ sleep-at-night/sleep-at-night.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -
 
+# Only suspend the system in the evening
+if [ $(date +"%H") -le 18 ]; then
+	echo "Not suspending system because it is too early"
+	exit 0
+fi
+
 # Only suspend when the system is idle
 # This is determined based on the average load during the last 15 min
 load=$(cat /proc/loadavg | cut -d " " -f 3)
 if [ ${load} \> 0.3 ]; then
-	echo "Not suspending as system is still busy ($load)"
+	echo "Not suspending system because it is still busy ($load)"
 	exit 0
 fi
 
